37684d8c7c430ffd567756f3e877fd4e094ebb90
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-09-25 14:05:05 -0300

    Create employee_registration.py

---
+++ employee_registration.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#exercise 01 @Marcos Augusto
+import math
+
+funcionario = {"nome": "", "cpf": "", "funcao": "", "remuneracao": int, "carga_horaria": ""}
+event = {"nome": "", "tipo": ""}
+lancamento = {"funcionario": "", "evento": "", "valor": int}
+
+op = ""
+
+while op != "sim":
+    funcionario["nome"] = input("Nome do funcionário: ")
+    funcionario["cpf"] = input("CPF: ")
+    funcionario["funcao"] = input("Função: ")
+    funcionario["remuneracao"] = float(input("Remuneração: "))
+    funcionario["carga_horaria"] = input("Carga horária: ")
+
+    print(funcionario)
+
+    eventop = input("Adicionar um evento? ")
+
+    if eventop == "sim":
+      event["nome"] = input("Nome do evento: ")
+      event["tipo"] = input("Tipo do evento: ")
+
+    lancop = input("Adicionar um lançamento? ")
+
+    if lancop == "sim":
+      lancamento["funcionario"] = input("Funcionário: ")
+      lancamento["evento"] = input("Evento: ")
+      lancamento["valor"] = float(input("Valor R$ "))
+
+    if lancamento["funcionario"] == funcionario["nome"] and event["tipo"] == "desconto":
+      if event["nome"] == "falta":
+         funcionario["remuneracao"] = funcionario["remuneracao"] - lancamento["valor"]
+
+    print(funcionario)
+    op  = input("Deseja encerrar? ")
