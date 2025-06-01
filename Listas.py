0ff8f9a35c04c58282ce1b15c7e6467bf73bbff5
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-05-31 22:30:53 -0300

    list

---
+++ Listas.py
@@ -0,0 +1,21 @@
+list1 = ["Banana", "maçã", "manga"]
+
+#Para saber o tamanho da lista
+"""size = len(list1)
+print(size)"""
+
+#Para adicionar um elemento ao final da lista
+"""list1.append("Acerola")
+print(list1)"""
+
+"""if "maçã" in list1:
+    print("Maçã está na lista.")
+else:
+    print("Maçã não está na lista.")"""
+
+#Para remover elementos da lista, neste caso da posição 2 para trás.
+"""del list1[2:]
+print(list1)"""
+
+#Para apagar a lista inteira
+"""del list1[:]"""
