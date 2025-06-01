67bb8060ff09544528956a162d7136ba4301fb9c
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-05-31 22:33:52 -0300

    random

---
+++ short_codes/random_1.py
@@ -0,0 +1,14 @@
+import random
+
+#Método randint: Gera um núemro aletório.
+
+"""number = random.randint(0, 10)
+print(number)"""
+
+#Método choice: Escolhe entre um dos valores informados(strings, números, etc).
+
+"""list = ["Marcos", "Augusto", "Rodrigues", "De", "Menezes"]
+number = random.choice(list)
+print(number)"""
+
+
