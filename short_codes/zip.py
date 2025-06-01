2762cee9f54ef55a7bd99feff2973e4ea24a871b
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-05-31 23:24:45 -0300

    Update zip.py

--- short_codes/zip.py
+++ short_codes/zip.py
@@ -1,8 +1,9 @@
 #A função zip concatena duas listas
 
-list1 = ["First_Name", "Second_Name", "Third_Name", "Fourth_Name", "Last_Name"]
-list2 = ["Marcos", "Augusto", "Rodrigues", "De", "Menezes"]
+list1 = [1, 2, 3, 4, 5]
+list2 = ["First_Name", "Second_Name", "Third_Name", "Fourth_Name", "Last_Name"]
+list3 = ["Marcos", "Augusto", "Rodrigues", "De", "Menezes"]
 
 #Pega o 1º elemento da ista 1 e concatena com o 1º elemento a lista 2 e assim por diante
-for position, name in  zip(list1, list2):
-    print(position, name)
+for index, position, name in  zip(list1, list2, list3):
+    print(index, position, name, )
