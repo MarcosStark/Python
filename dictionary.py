35b822d1bd6b39a612e35daae7105a593f441715
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-05-31 22:28:19 -0300

    disctionary

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+#Dicionário
+
+meu_nome = {"A":"MARCOS", "B":"AUGUSTO" }
+
+"""print(meu_nome["A"] + " " + meu_nome["B"])"""
+
+#Usando laço de repetição
+
+"""for key in meu_nome:
+    print(key + "-" + meu_nome[key])"""
+
+#Funções
+
+#items(): Retorna todos os itens do dicionáro. Conevrte o dicionário em uma tupla(Conjunto de dados imutáteis).
+
+"""for key in meu_nome.items():
+    print(key)"""
+
+#values(): Retorna os valores das chaves.
+
+"""for key in meu_nome.values():
+    print(key)"""
