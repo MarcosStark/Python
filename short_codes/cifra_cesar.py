2348a455b8c5e5963f33475bb6478396439fb5d8
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-09 01:56:49 -0300

    cifra_update

--- short_codes/cifra_cesar.py
+++ short_codes/cifra_cesar.py
@@ -1,16 +1,16 @@
 alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 key = ""
-input_option = ""
+input_value = ""
 
-while input_option != "enviar":
+while input_value != "enviar":
     input_value = input("Digite a letra: ")
-    input_option = input("Digite 'enviar' para finalizar ou 'continuar' para permanecer escrevendo: ")
-    if (input_option !="enviar") and (input_option !="continuar"):
-        print("Opção inválida.")
-    else:
-        key = str(key) + alphabet[alphabet.index(input_value) - 4]
 
-for i in key:
-    print(i)    
+    if input_value != "enviar":
+        key = str(key) + alphabet[alphabet.index(input_value) - 3]
+    else:
+        for i in key:
+            print(i, end="")
+
+
     
 
