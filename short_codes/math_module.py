e2f9d5c41ce7590152a6b4cb4150ab2d8edb3218
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-12 12:15:42 -0300

    more_practices

---
+++ short_codes/math_module.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.ceil(4.2)) #retorna o teto, no caso 5.
+
+print(math.floor(4.8)) #retorna o chão, no caso 4.
+
+print(math.pow(3, 2)) #retorna a exponenciação.
+
+print(math.sqrt(9)) #retorna a raíz quadrada.
+
+#Para mais, consultar documentação.
---
+++ short_codes/operation_tips.py
@@ -0,0 +1,23 @@
+num1 = 26.689
+
+print(num1)
+print(round(num1)) #retorna o arredondamento
+print(round(num1, 2)) #retorna o arredondamento com duas casas
+
+print(pow(3, 2)) #retorna a potência
+
+print(max(2, 4, 9, 100, 200, 50, 70))#retorna o maior número
+print(min(2, 4, 9, 100, 200, 50, 70))#retorna o menor
+
+print(sum([5, 9, 6]))#retorna a soma de todos os núemros
+
+#Ordem dos operadores
+
+#1. Parênteses ()
+#2. Exponenciação **
+#3. Multiplicação *
+#4. Divisão /
+#5. Divisão Inteira //
+#6. Módulo %
+#7. Adição +
+#8. Subtração -
