c9c6cc8a4bb1f419e12bb9c6cb7265a3827e8791
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-14 17:19:57 -0300

    for_loop_update

--- short_codes/basic/for_loop.py
+++ short_codes/basic/for_loop.py
@@ -1,6 +1,15 @@
-i = 10
+"""i = 10
 
 if i == 10:
     for n in range(0, 300, 10): #10: Intervalo entre os numeros impressos.
         print(n)
-        break #Interrompe na primeiro loop.
+        break #Interrompe na primeiro loop."""
+
+#Criando matriz
+bd = []
+
+"""for l in range(1, 4): #Loop da linha.
+    for c in range(1, 4): #Loop da coluna.
+        print(l, c)"""
+
+
