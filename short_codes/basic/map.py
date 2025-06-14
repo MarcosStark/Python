78e107a45460c7a6f52fed4da24d0cc38990d409
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-14 20:58:23 -0300

    Update map.py

--- short_codes/basic/map.py
+++ short_codes/basic/map.py
@@ -1,6 +1,6 @@
 #def: Palavra reservada para criar função
 def double(x):
-    return x*2
+    return x * 2
 
 list1 = [1, 2, 3, 4, 5]
 
@@ -14,3 +14,9 @@ for i in double_value:
 #Ou retorno direto como lista
 """double_value = list(double_value)
 print(double_value)"""
+
+print(list(map(lambda x: x * 2, list1))) #Tudo resumido em uma linha utilizando lambda.
+
+#list: Converte o resultado em lista.
+#map: Executada a funcao lambda item por item.
+#list1: Lista que sera percorrida.
