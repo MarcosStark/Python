7439f64c897f2a6c73715cc532142d632d9b84ec
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-14 19:38:47 -0300

    set

---
+++ short_codes/basic/set.py
@@ -0,0 +1,10 @@
+list1 = [10, 30, 50, 100]
+list2 = [20, 30, 40, 50, 90]
+
+result1 = set(list1)
+result2 = set(list2)
+
+print(result1 | result2) #Union: Retorna a junçao das listas, sem repetir numeros.
+print(result1 & result2) #And: Retorna o que existe nas duas listas.
+print(result1 ^ result2) #Symetric Difference: Retorna os valores, excetuando os que existem nas duas listas.
+print(result1 - result2) #Difference: Retorna o que existe na primeira lista e nao existe na segunda lista.
