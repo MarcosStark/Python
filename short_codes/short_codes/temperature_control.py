fb64cdc3824e5c9f14d373f9c42d4fef60ffd37c
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-14 10:27:08 -0300

    temp_update

--- short_codes/short_codes/temperature_control.py
+++ short_codes/short_codes/temperature_control.py
@@ -2,7 +2,7 @@ temp = float(input("Enter the temperature in Celsius: "))
 
 if temp < 10:
     print("O clima está muito frio. Agasalhe-se.")
-elif temp < 20 and temp > 10:
+elif temp <= 20 and temp >= 10:
     print("O clima está fresco. Convém algum agasalho, se não gosta de frio.")
 else:
     print("O clima está quente.")
