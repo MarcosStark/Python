8d19ec0adf9ef6f2c3485d36d9f6f8b91a2ee458
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-14 20:58:33 -0300

    Update set.py

--- short_codes/basic/set.py
+++ short_codes/basic/set.py
@@ -5,6 +5,14 @@ result1 = set(list1)
 result2 = set(list2)
 
 print(result1 | result2) #Union: Retorna a junçao das listas, sem repetir numeros.
-print(result1 & result2) #And: Retorna o que existe nas duas listas.
+print(result1 & result2) #Intersection: Retorna o que existe nas duas listas.
 print(result1 ^ result2) #Symetric Difference: Retorna os valores, excetuando os que existem nas duas listas.
 print(result1 - result2) #Difference: Retorna o que existe na primeira lista e nao existe na segunda lista.
+
+print("Methods")
+print(result1.union(result2))
+print(result1.intersection(result2))
+print(result1.symmetric_difference(result2))
+print(result1.difference(result2))
+
+#Consultar documentacao para mais.
