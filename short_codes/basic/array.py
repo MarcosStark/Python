f8d1da6d7e2ac12639a9c409eada84b2e7dc1edf
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-14 19:37:49 -0300

    array

---
+++ short_codes/basic/array.py
@@ -0,0 +1,5 @@
+#Deve ser usada quando a lista e muito grande e ha problema de performance no codigo.
+from array import array
+
+letters = array("w", ["m", "a", "r", "c", "o", "s"]) #Deve ser utilizado o unicode(Tabela disponivel na documentacao), nesse caso o 'w', representando string.
+print(letters)
