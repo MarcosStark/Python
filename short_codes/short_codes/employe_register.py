d06e5ea1e14527dda5de30f51852a4a3f5c80e71
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2025-06-16 11:43:42 -0300

    employe_register

--- short_codes/short_codes/employe_register.py
+++ short_codes/short_codes/employe_register.py
@@ -1,4 +1,4 @@
-#Cria a classe e o construttor, com seus atributos.
+#Cria a classe e o construtor, com seus atributos.
 class Employers:
     def __init__(self, name, last_name, age, salary):
         self.name = name
@@ -6,12 +6,16 @@ class Employers:
         self.age = age
         self.salary = salary
 
-    #method
+    #Method que retorna o nome completo.
     def full_name(self):
         return f"O nome do(a) colaborador(a) é {self.name} {self.last_name}."
     
 employer1 = Employers("Marcos", "Menezes", 22, 2500)
 employer2 = Employers("Alan", "Marcelino",25, 3000)
 
-print(employer1.full_name())
-print(employer2.full_name())
+#print(employer1.full_name())
+#print(employer2.full_name())
+
+#Printando as innformações de nome completo dos usuários.
+print(Employers.full_name(employer1))
+print(Employers.full_name(employer2))
